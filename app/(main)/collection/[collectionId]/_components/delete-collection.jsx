70c9a21d58eda5e33eb661561e2a038c2c723ccf
Vysavya <163625527+vysuvysavya@users.noreply.github.com>
2025-01-13 15:58:42 +0530

    Update delete-collection.jsx

--- app/(main)/collection/[collectionId]/_components/delete-collection.jsx
+++ app/(main)/collection/[collectionId]/_components/delete-collection.jsx
@@ -39,7 +39,6 @@ export default function DeleteCollectionDialog({
       );
       router.push("/dashboard");
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [deletedCollection, isDeleting]);
 
   const handleDelete = async () => {
